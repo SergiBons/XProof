5fce7805270227c4e22fab28a4ded6c85b0e1e86
SergiBons <57765195+SergiBons@users.noreply.github.com>
2022-10-17 19:52:06 +0200

    Add files via upload

---
+++ Xproof/src/PortableControllerTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PortableControllerTest {
+
+	@Test
+	void testAddToList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveFromList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckListNameFromDatabase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveList() {
+		fail("Not yet implemented");
+	}
+
+}
