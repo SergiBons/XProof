25f9f72fe7b77a6c93a6e7066c76ca2191841e60
SergiBons <57765195+SergiBons@users.noreply.github.com>
2022-11-14 12:33:32 +0100

    View Functions declared

--- Xproof/src/MainScreenView.java
+++ Xproof/src/MainScreenView.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class MainScreenView {
-
+	public void UpdateListView(HashMap<String, String[]> ListMap) {
+		clearScreen();
+		for (String NomLlista :ListMap.keySet()) {
+			System.out.print("--------------------");
+			System.out.print(NomLlista);
+			System.out.print(ListMap.get(NomLlista).length);
+			System.out.print("--------------------");
+		}
+	}
+	public String[] LoginView(){
+		String[] def = {"",""};
+		return def;
+	}
+	
+	public String[] ReisterView(){
+		String[] def = {"",""};
+		return def;
+	}
+	
+	public boolean LoginOrRegister(){
+		return true;
+	}
+	
+	public static void clearScreen() {  
+	    System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	}  
 }
