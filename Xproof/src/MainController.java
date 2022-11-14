2b5a96b7533c8d0dcf40662fb3f056dd46d6acb9
SergiBons <57765195+SergiBons@users.noreply.github.com>
2022-11-14 20:18:00 +0100

    MainController declared

---
+++ Xproof/src/MainController.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MainController {
+	public void main(String[] args) {
+
+	}
+	public PortableController Log_Reg(MainScreenView MSV, PortableController PC,ModelRegister RDB,String err)
+	{
+		
+		return PC;
+	}
+	public PortableController TerminalInteract(PortableController PC, int s) {
+
+		return PC;
+	}
+	
+	public int SelectorPick(PortableController PC, MainScreenView MSV) {
+		int selector = 15;
+
+		return selector;
+	}
+}
+
+
+
